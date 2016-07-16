b0b8185a144659e1cc11b29881ed2bfb422201dc
Joel de Santos <joeldesantos@gmail.com>
2016-07-16 14:33:00 -0500

    Add files via upload

---
+++ class05_3_noise.m
@@ -0,0 +1,14 @@
+close all; clear all;
+
+I1 = imread('..\images\lena512color.tiff');
+%I1 = imread('cameraman.tif'); % does not work
+%I1 = imread('peppers.png');
+f = rgb2gray(I1);
+
+J = imnoise(f,'salt & pepper',0.02);
+K = medfilt2(J);
+
+subplot(2,2,1); imshow(K,[]); 
+subplot(2,2,2); imshow(J,[]); % this image will have a black border
+%subplot(2,2,3); imshow(Hf,[]);
+%subplot(2,2,4); imshow(g,[]);
